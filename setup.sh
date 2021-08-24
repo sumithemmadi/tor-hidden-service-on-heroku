ae6159dc386cc4080b5bf7956ea36e9fc16b473a
Emmadi Sumith Kumar <50250422+sumithemmadi@users.noreply.github.com>
2021-08-24 08:26:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-rm -f /app/temp/etc/tor/torrc_${1}
-echo "HiddenServiceDir /app/temp/var/lib/tor/hidden_service" >> /app/temp/etc/tor/torrc_${1}
-echo "HiddenServicePort 80 127.0.0.1:${1}" >> /app/temp/etc/tor/torrc_${1} 
+PORT1=`echo $PORT`
+rm -f /app/temp/etc/tor/torrc_$PORT1
+echo "HiddenServiceDir /app/temp/var/lib/tor/hidden_service" >> /app/temp/etc/tor/torrc_$PORT1
+echo "HiddenServicePort 80 127.0.0.1:$PORT1" >> /app/temp/etc/tor/torrc_$PORT1
